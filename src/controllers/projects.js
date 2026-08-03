fc1e432d5bb11fa0b2e8c59b57a36d897fda678d
emerald067 <eawoke@byupathway.edu>
2026-08-03 18:33:06 +0100

    Week 03 Learning Activities Complete!

---
+++ src/controllers/projects.js
@@ -0,0 +1,33 @@
+import {
+  getUpcomingProjects,
+  getProjectDetails,
+} from "../models/projects.js";
+
+const NUMBER_OF_UPCOMING_PROJECTS = 5;
+
+const showProjectsPage = async (req, res) => {
+  const projects = await getUpcomingProjects(
+    NUMBER_OF_UPCOMING_PROJECTS
+  );
+
+  res.render("projects", {
+    title: "Upcoming Service Projects",
+    projects,
+  });
+};
+
+const showProjectDetailsPage = async (req, res) => {
+  const id = req.params.id;
+
+  const project = await getProjectDetails(id);
+
+  res.render("project", {
+    title: project.title,
+    project,
+  });
+};
+
+export {
+  showProjectsPage,
+  showProjectDetailsPage,
+};
--- src/models/projects.js
+++ src/models/projects.js
@@ -1,16 +1,91 @@
-import db from './db.js';
+import db from "./db.js";
 
 const getAllProjects = async () => {
-    const query = `
-        SELECT p.project_id, p.title, p.project_date, p.description, p.location, o.name AS organization_name
-        FROM public.project AS p
-        INNER JOIN public.organization AS o ON p.organization_id = o.organization_id
-        ORDER BY p.project_date ASC;
-    `;
+  const query = `
+    SELECT
+      p.project_id,
+      p.organization_id,
+      p.title,
+      p.description,
+      p.location,
+      p.project_date,
+      o.name AS organization_name
+    FROM project p
+    JOIN organization o
+      ON p.organization_id = o.organization_id
+    ORDER BY p.project_date ASC;
+  `;
 
-    const result = await db.query(query);
-
-    return result.rows;
+  const result = await db.query(query);
+  return result.rows;
 };
 
-export { getAllProjects };
+const getUpcomingProjects = async (numberOfProjects) => {
+  const query = `
+    SELECT
+      p.project_id,
+      p.organization_id,
+      p.title,
+      p.description,
+      p.location,
+      p.project_date,
+      o.name AS organization_name
+    FROM project p
+    JOIN organization o
+      ON p.organization_id = o.organization_id
+    WHERE p.project_date >= CURRENT_DATE
+    ORDER BY p.project_date ASC
+    LIMIT $1;
+  `;
+
+  const result = await db.query(query, [numberOfProjects]);
+
+  return result.rows;
+};
+
+const getProjectDetails = async (id) => {
+  const query = `
+    SELECT
+      p.project_id,
+      p.organization_id,
+      p.title,
+      p.description,
+      p.location,
+      p.project_date,
+      o.name AS organization_name
+    FROM project p
+    JOIN organization o
+      ON p.organization_id = o.organization_id
+    WHERE p.project_id = $1;
+  `;
+
+  const result = await db.query(query, [id]);
+
+  return result.rows[0];
+};
+
+const getProjectsByOrganizationId = async (organizationId) => {
+  const query = `
+    SELECT
+      project_id,
+      organization_id,
+      title,
+      description,
+      location,
+      project_date
+    FROM project
+    WHERE organization_id = $1
+    ORDER BY project_date;
+  `;
+
+  const result = await db.query(query, [organizationId]);
+
+  return result.rows;
+};
+
+export {
+  getAllProjects,
+  getUpcomingProjects,
+  getProjectDetails,
+  getProjectsByOrganizationId,
+};
